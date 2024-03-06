6deb9fc6ac41fb32fae2e3196ce6e0cf92ca2997
TimeLovercc <gzjz07@outlook.com>
2024-03-06 18:28:41 -0500

    some

--- plugins/user.lua
+++ plugins/user.lua
@@ -73,5 +73,12 @@ return {
       server_opts_overrides = {},
       })
     end,
-  } 
+  },
+  {
+    'adelarsq/image_preview.nvim',
+    event = 'VeryLazy',
+    config = function()
+        require("image_preview").setup()
+    end
+  }
 }
